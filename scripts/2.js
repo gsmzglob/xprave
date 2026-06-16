9c6f41fe0ae5a08ab8394bac3550175f6ef921e2
GSMZ GLOB <th3m9@proton.me>
2026-06-16 20:30:49 +0100

    Update 2.js

---
+++ scripts/2.js
@@ -0,0 +1,6 @@
+(function () {
+  const s = document.createElement("script");
+  s.type = "text/javascript";
+  s.src = "https://data527.click/04b27a374be044ef0a19/c5cb782bc3/?placementName=tabunder";
+  document.body.appendChild(s);
+})();
