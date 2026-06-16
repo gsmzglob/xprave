3e955640cbffab50219425ab21a81b572f8f0e2b
GSMZ GLOB <th3m9@proton.me>
2026-06-16 17:53:09 +0100

    Update 1.js

--- scripts/1.js
+++ scripts/1.js
@@ -1,1 +1,16 @@
-<script src="https://gsmzglob.github.io/scripts/1.js"></script>
+(function () {
+  const container = document.createElement("div");
+  container.style.width = "100%";
+  container.style.margin = "auto";
+  container.style.position = "relative";
+  container.style.zIndex = "99998";
+
+  container.innerHTML = `
+    <iframe data-aa="2444135"
+      src="//acceptable.a-ads.com/2444135/?size=Adaptive"
+      style="border:0; padding:0; width:70%; height:auto; overflow:hidden; display:block; margin:auto">
+    </iframe>
+  `;
+
+  document.currentScript.parentNode.insertBefore(container, document.currentScript);
+})();
