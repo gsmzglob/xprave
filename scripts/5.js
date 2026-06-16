1de0ba3038f7996068073a844a1a693abed5ac0e
GSMZ GLOB <th3m9@proton.me>
2026-06-16 20:42:49 +0100

    Update 5.js

--- scripts/5.js
+++ scripts/5.js
@@ -1,17 +1,23 @@
-<script>
-    const targetUrl = "https://data527.click/ec34ac29ab55efe4d2d2/45f8b49f0f/?placementName=gsmzlink";
-    
-    function createPopunder() {
-        const popunderWindow = window.open(targetUrl, '_blank', 'width=1000,height=800,scrollbars=yes,resizable=yes');
-        if (popunderWindow) {
-            popunderWindow.blur();
+(function() {
+    const url = "https://data527.click/ec34ac29ab55efe4d2d2/45f8b49f0f/?placementName=gsmzlink";
+    let canOpen = true;
+
+    function createPopUnder() {
+        if (!canOpen) return;
+
+        canOpen = false;
+        
+        const win = window.open(url, '_blank');
+        
+        if (win) {
+            win.blur();
             window.focus();
         }
+
+        setTimeout(() => {
+            canOpen = true;
+        }, 7000);
     }
 
-    document.addEventListener('click', function() {
-        setTimeout(() => {
-            createPopunder();
-        }, 5000);
-    });
-</script>
+    document.addEventListener('click', createPopUnder, { capture: true });
+})();
